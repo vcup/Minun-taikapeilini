ddc14a2c7d23266f5b03d8b105d6f55c52900d95
miryo <miryo@vcup.moe>
2023-06-27 19:49:22 +0800

    feat: scroll wheel to switch current page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-start">
+  <div class="flex justify-start w-screen h-screen" @wheel="handleScroll">
     <UVerticalNavigation :links="links" class="w-96 px-5 py-32" />
     <slot />
   </div>
@@ -11,11 +11,24 @@ const links = [
     label: 'Home',
     icon: 'i-heroicons-home',
     to: '/',
-  }, 
+  },
   {
     label: 'WorldMap',
     icon: 'i-heroicons-map',
     to: '/world-map',
   }
 ]
+const router = useRouter()
+function handleScroll(event: WheelEvent) {
+  if (event.ctrlKey || event.altKey || event.shiftKey) return
+  const currentPath = router.currentRoute.value.path
+  const isScrollDown = event.deltaY > 0;
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].to !== currentPath) continue
+    if (isScrollDown && ++i >= links.length) break
+    if (!isScrollDown && --i < 0) break
+    navigateTo(links[i].to, { replace: true })
+    break
+  }
+}
 </script>
