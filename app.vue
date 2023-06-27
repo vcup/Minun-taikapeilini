c214b2cb39c3029b453fc6fdf114803efd9df3bf
miryo <miryo@vcup.moe>
2023-06-27 18:51:45 +0800

    feat: add layouts and pages

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <main>
+      <NuxtPage />
+    </main>
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex justify-start">
+    <UVerticalNavigation :links="links" class="w-96 px-5 py-32" />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+const links = [
+  {
+    label: 'Home',
+    icon: 'i-heroicons-home',
+    to: '/',
+  }, 
+  {
+    label: 'WorldMap',
+    icon: 'i-heroicons-map',
+    to: '/world-map',
+  }
+]
+</script>
